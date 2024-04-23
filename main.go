c4c67300812746530c7dbacc14731e465893db4a
bdzhalalov <bahadzhalalov@gmail.com>
2024-04-23 19:22:10 +0500

    rename directory for project utils

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"log"
 	"motivations-api/config"
-	"motivations-api/lib"
+	"motivations-api/internal"
 )
 
 func main() {
 	cfg := config.InitConfig()
 
-	server := lib.Init(&cfg)
+	server := internal.Init(&cfg)
 
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
